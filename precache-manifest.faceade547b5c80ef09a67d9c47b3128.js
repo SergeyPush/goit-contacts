6dc1c784410fea0aa25b03cb1ac5577ac4b67cae
Sergey Pushkovskiy <push_s@mail.ua>
2019-11-03 19:03:28 +0200

    Updates

---
+++ precache-manifest.faceade547b5c80ef09a67d9c47b3128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a20aeb1f44f09b4f487178d771d8680",
+    "url": "/goit-contacts/index.html"
+  },
+  {
+    "revision": "9902d6f08a0cf162f266",
+    "url": "/goit-contacts/static/css/main.53e04405.chunk.css"
+  },
+  {
+    "revision": "f7165024281a95591600",
+    "url": "/goit-contacts/static/js/2.b1b26279.chunk.js"
+  },
+  {
+    "revision": "9902d6f08a0cf162f266",
+    "url": "/goit-contacts/static/js/main.0a794147.chunk.js"
+  },
+  {
+    "revision": "9354a654de3528c6419e",
+    "url": "/goit-contacts/static/js/runtime-main.8de3c2ac.js"
+  }
+]);
